93fabb6bc87645d7171bca8e30dca09c4e00e1aa
Giuse-04 <gmancinelli@fi.uba.ar>
2026-04-26 18:52:37 -0300

    cambio en el gitignore para NO ignorar el \lib - Estos cambios son del commit anterior mio en realidad - Constantes y mover de lugar archivos.

---
+++ src/lib/protocol_sender/stop_and_wait.py
@@ -0,0 +1,45 @@
+import socket
+from lib.constants import PACKET_PAYLOAD_SIZE, ACK_BUFFER_SIZE
+from lib.protocols.base import TransferStrategy
+from lib.datagrams.datagram import Datagram
+from lib.datagrams.data import DataDatagram
+from lib.datagrams.ack import AckDatagram
+
+
+class StopAndWaitStrategy(TransferStrategy):
+    def transfer(self, local_path: str, seq_num: int) -> int:
+        self.logger.debug("Starting [Stop & Wait] Transfer...")
+        self.sock.setblocking(True)
+        self.sock.settimeout(self.timeout_limit)
+
+        with open(local_path, "rb") as file:
+            while True:
+                block = file.read(PACKET_PAYLOAD_SIZE)
+                if not block:
+                    break
+
+                data_packet = DataDatagram(seq_num, block)
+                packet_bytes = data_packet.to_bytes()
+                ack_received = False
+                attempts = 0
+
+                while not ack_received and attempts < self.max_attempts:
+                    self.sock.sendto(packet_bytes, self.server_addr)
+                    try:
+                        data, _ = self.sock.recvfrom(ACK_BUFFER_SIZE)
+                        response = Datagram.from_bytes(data)
+
+                        if isinstance(response, AckDatagram) and response.seq_num == seq_num:
+                            ack_received = True
+                            seq_num += 1
+                        else:
+                            continue
+
+                    except socket.timeout:
+                        attempts += 1
+                        self.logger.debug(f"S&W: Timeout for block {seq_num}. Retry {attempts}")
+
+                if not ack_received:
+                    raise ConnectionError(f"Connection lost while sending block {seq_num}")
+
+        return seq_num
