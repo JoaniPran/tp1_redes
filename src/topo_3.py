80627c4dcf948db00d5a9338752b063c96d893ba
Gonzalo <llaos@fi.uba.ar>
2026-05-05 15:37:08 -0300

    agrego topo_3 para descargas

--- src/topo_3.py
+++ src/topo_3.py
@@ -5,13 +5,12 @@ from mininet.log import setLogLevel
 import time
 import os
 
-
 def run_topology():
-    # Obtener el directorio donde está topo.py
+    # Obtener el directorio donde esta topo.py
     script_dir = os.path.dirname(os.path.abspath(__file__))
 
-    # Crear Mininet sin controlador
-    net = Mininet(link=TCLink)
+    # Crear Mininet SIN controlador (Correccion para Ubuntu)
+    net = Mininet(controller=None, link=TCLink)
 
     print("*** Añadiendo hosts (Servidor y Cliente)")
     server = net.addHost('h1', ip='10.0.0.1/24')
@@ -22,7 +21,7 @@ def run_topology():
     print("*** Añadiendo Switch")
     switch = net.addSwitch('s1', failMode='standalone')
 
-    print("*** Creando enlaces con 5% de pérdida en cada pata")
+    print("*** Creando enlaces con 5% de perdida en cada pata")
     net.addLink(server, switch, loss=5)
     net.addLink(client1, switch, loss=5)
     net.addLink(client2, switch, loss=5)
@@ -43,18 +42,19 @@ def run_topology():
 
     time.sleep(1)
 
-    print("*** [Test] Disparando los 3 clientes de forma simultánea...")
-    client1.cmd('python3 upload -H 10.0.0.1 -p 8080 -s ../documentos/test_15mb.bin -n test_15mb.bin -r sr -v > h2_upload.log 2>&1 &')
-    client2.cmd('python3 upload -H 10.0.0.1 -p 8080 -s ../documentos/test_15mb_2.bin -n test_15mb_2.bin -r sr -v > h3_upload.log 2>&1 &')
-    client3.cmd('python3 upload -H 10.0.0.1 -p 8080 -s ../documentos/test_15mb_3.bin -n test_15mb_3.bin -r sr -v > h4_upload.log 2>&1 &')
-    print("*** [Test] Transferencias iniciadas. Revisa los .log para ver el progreso.")
+    print("*** [Test] Disparando los 3 clientes para DESCARGA simultanea...")
+    # Asegurate de que los flags '-n' (remoto) y '-d' (destino local) coincidan con el parser de tu client_download.py
+    client1.cmd('python3 download -H 10.0.0.1 -p 8080 -d ./descargas/down_1.bin -n test_15mb.bin -r sr -v > h2_dowload.log 2>&1 &')
+    client2.cmd('python3 download -H 10.0.0.1 -p 8080 -d ./descargas/down_2.bin -n test_15mb_2.bin -r sr -v > h3_dowload.log 2>&1 &')
+    client3.cmd('python3 download -H 10.0.0.1 -p 8080 -d ./descargas/down_3.bin -n test_15mb_3.bin -r sr -v > h4_dowload.log 2>&1 &')
+    
+    print("*** [Test] Descargas iniciadas. Revisa los .log para ver el progreso.")
 
     print("*** Entrando a la consola interactiva")
     CLI(net)
 
     print("*** Deteniendo la red")
     net.stop()
-
 
 if __name__ == '__main__':
     setLogLevel('info')
